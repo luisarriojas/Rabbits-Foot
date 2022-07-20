0d6e0ad1fcef3d1655f195c112e0587fd04f354a
Luis Arriojas <luisarriojas@gmail.com>
2022-07-20 13:14:19 -0400

    added connection to MongoDB

--- phase1.js
+++ phase1.js
@@ -1,22 +1,41 @@
 const https = require("https");
 const cheerio = require('cheerio');
+const {MongoClient} = require('mongodb');
 
-//obtener link de la DB.
-let options = new URL('https://www.romspedia.com/roms/super-nintendo');
+async function main() {
+    console.clear();
 
-let req = https.request(options, function(res) {
-    let content = "";
-    res.setEncoding("utf8");
+    const uri = "mongodb://localhost:27017";
+    const client = new MongoClient(uri);
+    try {
+        await client.connect().then(getHtml());
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close(); 
+    }
+}
 
-    res.on("data", function (chunk) {
-        content += chunk;
-    });
+function getHtml() {
+    //obtener link de la DB.
+    let options = new URL('https://www.romspedia.com/roms/super-nintendo');
 
-    res.on("end", function () {
-        const $ = cheerio.load(content);
-        $('div.roms-img a').get().forEach((link) => {
-            console.log(link.attribs.href);
+    let req = https.request(options, function(res) {
+        let content = "";
+        res.setEncoding("utf8");
+
+        res.on("data", function (chunk) {
+            content += chunk;
+        });
+
+        res.on("end", function () {
+            const $ = cheerio.load(content);
+            $('div.roms-img a').get().forEach((link) => {
+                console.log(link.attribs.href);
+            });
         });
     });
-});
-req.end();
+    req.end();
+};
+
+main();
