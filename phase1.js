e0b2a8d1441c3df00dabd05521240f7ec587fd3e
Luis Arriojas <luisarriojas@gmail.com>
2022-07-24 14:30:15 -0400

    Save links of a specific page into DB.

--- phase1.js
+++ phase1.js
@@ -4,37 +4,42 @@ const {MongoClient} = require('mongodb');
 
 async function main() {
     console.clear();
-
     const uri = "mongodb://localhost:27017";
-    const client = new MongoClient(uri);
-    try {
-        await client.connect().then(getHtml());
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close(); 
-    }
+    new MongoClient(uri).connect()
+        .then(mongoClient => getHtml(mongoClient))
+        .catch(e => console.log(e));
 }
 
-function getHtml() {
-    //obtener link de la DB.
+function getHtml(mongoClient) {
+    //get link from DB.
     let options = new URL('https://www.romspedia.com/roms/super-nintendo');
 
-    let req = https.request(options, function(res) {
+    let req = https.request(options, (res) => {
         let content = "";
+    
         res.setEncoding("utf8");
-
-        res.on("data", function (chunk) {
+        res.on("data", (chunk) => {
             content += chunk;
         });
 
-        res.on("end", function () {
+        res.on("end", async () => {
+            const collection = mongoClient.db('rabbits-foot').collection('phase1');
             const $ = cheerio.load(content);
+            const linkList = [];
             $('div.roms-img a').get().forEach((link) => {
-                console.log(link.attribs.href);
+                linkList.push({
+                    url: link.attribs.href
+                });
             });
+
+            if (linkList.length > 0) {
+                await collection.insertMany(linkList);
+            }
+
+            await mongoClient.close(); 
         });
     });
+
     req.end();
 };
 
