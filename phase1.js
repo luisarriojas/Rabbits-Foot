1d62f8f4709c44bb8eb66dd2199a840606b4c4d5
Luis Arriojas <luisarriojas@gmail.com>
2022-07-29 23:10:35 -0400

    added else path to resolve promise of getHtml()

--- phase1.js
+++ phase1.js
@@ -41,6 +41,8 @@ async function getHtml(mongoClient) {
                         .then(() => {
                             resolve();
                         })
+                } else {
+                    resolve();
                 }
             });
         });
